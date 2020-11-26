38f0e7e410631bd56e47181c903b604a1d0fd757
huaimingxiang <kidhsks@hotmail.com>
2020-11-26 22:04:42 +0800

    modified:   functions.php 	modified:   style.css

--- functions.php
+++ functions.php
@@ -24,4 +24,22 @@ return "<a $text>";
 
 add_filter('wp_tag_cloud', 'colorCloud', 1);
 
+
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style'; 
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
 ?>
+
+
+
