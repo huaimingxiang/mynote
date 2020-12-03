3ec8c1cbcf1af19f3e1d09b6f003b677fbc5ae34
huaimingxiang <kidhsks@hotmail.com>
2020-12-03 09:41:59 +0800

    new file:   custom_share.php

---
+++ custom_share.php
@@ -0,0 +1,14 @@
+<div class="footer-nav">
+    <h6>分享<h6>
+    
+    <a target="_blank" rel="nofollow" title="Facebook"><span class="brand-sm"><i class="fab fa-facebook facebook brand-link"></i></span></a>
+    <!--href="http://v.t.sina.com.cn/share/share.php?&url=<?php the_permalink(); ?>&title=<?php the_title(''); ?>" -->
+    
+     <a target="_blank" rel="nofollow" title="twitter"><span class="brand-sm"><i class="fab fa-twitter twitter brand-link"></i></span></a>
+     <!--href="http://v.t.sina.com.cn/share/share.php?&url=<?php the_permalink(); ?>&title=<?php the_title(''); ?>" --> 
+
+    <a target="_blank" rel="nofollow" href="http://v.t.sina.com.cn/share/share.php?&url=<?php the_permalink(); ?>&title=<?php the_title(''); ?>"  title="新浪微博"><span class="brand-sm"><i class="fab fa-weibo weibo brand-link"></i></span></a>
+    
+    <a target="_blank" rel="nofollow" href="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=<?php the_permalink(); ?>&title=<?php the_title(''); ?>&desc=&summary=&site=" title="QQ空间"><span class="brand-sm"><i class="fab fa-qq qq brand-link"></i></span></a>
+    
+</div>  
